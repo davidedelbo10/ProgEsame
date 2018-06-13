80ebd2a549f93b63b9961afdd8733ceae19cc7ed
davidedelbo10 <davidedelbo.10@gmail.com>
2018-06-13 19:26:10 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var config = {
     password: "password.123",
     server: "delboverpelli.database.windows.net",
     options:
-    {
-        database: "ProgettoEsame",
-        encrypt: true
-    }
+        {
+            database: "ProgettoEsame",
+            encrypt: true
+        }
 }
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
@@ -39,13 +39,12 @@ function login(user, pass, reply) {
         }
         else {
             console.log('Connected');
-            var Risposta = null;
             var query = "SELECT IdU FROM Utenti WHERE Username=@user AND Password=@pass"; //CRIPTA LA PASSWORD
             var request = new Request(query, function (err, rowCount) {
                 if (err) { console.log(err); }
                 else {
                     if (rowCount = 0) {
-                        Risposta.push({ status: 'ko' });
+                        reply({ status: 'ko' });
                     }
                     reply(Risposta);
                 }
@@ -57,7 +56,7 @@ function login(user, pass, reply) {
 
             request.on('row', function (columns) {
                 columns.forEach(function (column) {
-                    Risposta.push({ status: 'ok', id: column.value })
+                    reply({ status: 'ok', id: column.value })
                 }
                 )
             });
@@ -87,18 +86,16 @@ function signup(user, pass, reply) {
         }
         else {
             console.log('Connected');
-            var Risposta = null;
             var query = "INSERT INTO Utenti(Username, Password) VALUES (@user,@pass)"; //DA CRIPTARE
             var request = new Request(query, function (err, rowCount) {
                 if (err) { console.log(err); }
                 else {
                     if (rowCount = 0) {
-                        Risposta.push({ status: 'ko' });
+                        reply({ status: 'ko' });
                     }
                     else {
-                        Risposta.push({ status: 'ok' });
+                        reply({ status: 'ok' });
                     }
-                    reply(Risposta);
                 }
             });
             request.addParameter('user', TYPES.VarChar, user);
@@ -120,46 +117,43 @@ server.route({
     }
 });
 
-function TrovaPunteggio(user,reply)
-{
-
+function TrovaPunteggio(user, reply) {
+    var punti = null;
     var connection = new Connection(config);
-    
-            connection.on('connect', function (err) {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    console.log('Connected');
-                    var Risposta = null;
-                    var query = "SELECT Punteggio FROM Utenti WHERE Username=@user";
-                    var request = new Request(query, function (err, rowCount) {
-                        if (err) { console.log(err); }
-                        else {
-                            if (rowCount = 0) {
-                                Risposta.push({ status: 'ko' });
-                            }
-                            else {
-                                Risposta.push({ status: 'ok' });
-                            }
-                            reply(Risposta);
-                        }
-                    });
-                    
-                    request.addParameter('user', TYPES.VarChar, user);
 
-                    request.on('row', function (columns) {
-                        columns.forEach(function (column) {
-                            Risposta.push({ status: 'ok', punteggio: column.value })
-                        }
-                        )
-                    });
-                    connection.execSql(request);
+    connection.on('connect', function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log('Connected');
+            var query = "SELECT Punteggio FROM Utenti WHERE Username=@user";
+            var request = new Request(query, function (err, rowCount) {
+                if (err) { console.log(err); }
+                else {
+                    if (rowCount = 0) {
+                        reply({ status: 'ko', punteggio: 0 });
+                    }
+                    else {
+                        reply({ status: 'ok', punteggio: punti });
+                    }
+                    reply(Risposta);
                 }
-            });                              
+            });
+
+            request.addParameter('user', TYPES.VarChar, user);
+
+            request.on('row', function (columns) {
+                columns.forEach(function (column) {
+                    punti = column.value;
+                });
+            });
+            connection.execSql(request);
+        }
+    });
 }
 
 server.start(function () {
     console.log('Hapi is listening');
 });
-                
+
